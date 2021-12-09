d0ce78c934ff46ba59191eeecdcf9026df0d91db
Dwi Joko Purnomo <you@example.com>
2021-12-09 14:35:45 +0700

    update code

--- js/aiden.js
+++ js/aiden.js
@@ -76,3 +76,52 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+// fixed element
+
+// var startProductBarPos = -1;
+// window.onscroll = function () {
+//   var bar = document.getElementById("banner-text");
+//   if (startProductBarPos < 0) startProductBarPos = findPosY(bar);
+
+//   if (pageYOffset > startProductBarPos) {
+//     bar.style.position = "fixed";
+//     bar.style.top = 0;
+//   } else {
+//     bar.style.position = "relative";
+//   }
+// };
+
+// function findPosY(obj) {
+//   var curtop = 0;
+//   if (typeof obj.offsetParent != "undefined" && obj.offsetParent) {
+//     while (obj.offsetParent) {
+//       curtop += obj.offsetTop;
+//       obj = obj.offsetParent;
+//     }
+//     curtop += obj.offsetTop;
+//   } else if (obj.y) curtop += obj.y;
+//   return curtop;
+// }
+
+// $(document).ready(function () {
+//   // Cache selectors for faster performance.
+//   var $window = $(window),
+//     $bannerText = $("#bannerText"),
+//     $bannerAnchor = $("#bannerAnchor");
+
+//   // Run this on scroll events.
+//   $window.scroll(function () {
+//     var window_top = $window.scrollTop();
+//     var div_top = $bannerAnchor.offset().top;
+//     if (window_top > div_top) {
+//       // Make the div sticky.
+//       $bannerText.addClass("stick");
+//       $bannerAnchor.height($bannerText.height());
+//     } else {
+//       // Unstick the div.
+//       $bannerText.removeClass("stick");
+//       $bannerAnchor.height(0);
+//     }
+//   });
+// });
